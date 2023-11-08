3e86e460697614e62b44f2032a7134d408e80710
Alan Darmasaputra <kelerchian@gmail.com>
2023-11-08 17:26:32 +0100

    add node10 moduleresolution support

---
+++ scripts/generate-node10-support.ts
@@ -0,0 +1,61 @@
+import * as fs from "node:fs/promises";
+import * as path from "node:path";
+
+(async () => {
+  const rootPath = path.resolve(".");
+  const packageJsonPath = path.resolve(rootPath, "package.json");
+  const packageJson = JSON.parse(await fs.readFile(packageJsonPath, "utf8"));
+  const exports = packageJson?.tshy?.exports;
+  if (!exports) throw new Error("tshy.exports undefinefd");
+
+  const subpaths = Object.entries(exports);
+  const pathConfigs = subpaths
+    .filter(([key, _]) => key !== "." && key !== "./package.json")
+    .map(([key, value]) => {
+      const sourcePathParsed = path.parse(value as string);
+      return {
+        pathExtless: path.relative(
+          "./src",
+          path.join(sourcePathParsed.dir, sourcePathParsed.name),
+        ),
+        supportDirPath: path.resolve(rootPath, key),
+      };
+    });
+
+  const tasks = pathConfigs.map((p) => async () => {
+    const toRoot = path.relative(p.supportDirPath, rootPath); // should be ".."
+    await fs.mkdir(p.supportDirPath, {
+      recursive: true,
+    });
+    await fs.writeFile(
+      path.join(p.supportDirPath, "package.json"),
+      JSON.stringify(
+        {
+          main: `${toRoot}/dist/commonjs/${p.pathExtless}.js`,
+          module: `${toRoot}/dist/esm/${p.pathExtless}.js`,
+          typings: `${toRoot}/dist/commonjs/${p.pathExtless}.d.ts`,
+        },
+        null,
+        2,
+      ),
+      "utf8",
+    );
+  });
+
+  for (const task of tasks) {
+    await task();
+  }
+
+  const oldPackageJsonFiles = new Set<string>(packageJson.files);
+  pathConfigs.forEach((p) => oldPackageJsonFiles.add(p.pathExtless));
+  packageJson.files = Array.from(oldPackageJsonFiles);
+
+  await fs.writeFile(
+    packageJsonPath,
+    JSON.stringify(packageJson, null, 2),
+    "utf8",
+  );
+})().catch((err) => {
+  console.error(err);
+  process.exit(1);
+});
